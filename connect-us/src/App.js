77084f7a3724391ebbf1c96e59ca9f7bf023ffde
Adekoya Samuel <99253410+Samdek@users.noreply.github.com>
2023-12-15 22:20:07 +0100

    setup

---
+++ connect-us/src/App.js
@@ -0,0 +1,12 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="h-screen flex items-center justify-center font-nunito">
+      <button className="bg-orange-600 text-white text-xl py-3 px-8 rounded-sm">Click me</button>
+    </div>
+  );
+}
+
+export default App;
