5193577db7ae7f8ff943a37ef0d781ba3c7dfe43
Adekoya Samuel <99253410+Samdek@users.noreply.github.com>
2023-12-20 23:35:04 +0100

    added news

--- connect-us/src/layout/DashboardLayout/News.js
+++ connect-us/src/layout/DashboardLayout/News.js
@@ -1,31 +1,35 @@
 import { useEffect, useState } from "react";
 import axios from 'axios';
 
+
     function News(){
   const [responseData, setResponseData] = useState(null);
+  const retrievedKey = localStorage.getItem('accessToken')
+
+  const headers = {
+    'Content-Type': 'application/json',
+    'Authorization': `Bearer ${retrievedKey}`,
+  };
 
   useEffect(() => {
     const fetchData = async () => {
       try {
-        const response = await axios.get("http://connectus-4ev0.onrender.com/news/all?page=1&limit=6&category=Holiday");
+        const response = await axios.get('http://connectus-4ev0.onrender.com/messages', { headers, });
 
-        // Handle the response
         console.log(response.data);
-      } catch (error) {
-        // Handle errors
-        console.log('Error:', error.message);
+      } catch (err) {
+        console.log('Error:', err.message);
       }
     };
 
     fetchData();
-  }, []); // Empty dependency array ensures the effect runs once on component mount
+  }, []);
 
   return (
     <div>
-      {/* Your component UI */}
       {responseData ? (
         <div>
-          <h1>Data from API:</h1>
+          <h1>Announcements</h1>
           <pre>{JSON.stringify(responseData, null, 2)}</pre>
         </div>
       ) : (
