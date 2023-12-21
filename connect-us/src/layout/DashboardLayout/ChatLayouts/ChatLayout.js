fd43ce83e59bfde97bf097c77e7daf8746a27708
Adekoya Samuel <99253410+Samdek@users.noreply.github.com>
2023-12-21 04:54:23 +0100

    updated

--- connect-us/src/layout/DashboardLayout/ChatLayouts/ChatLayout.js
+++ connect-us/src/layout/DashboardLayout/ChatLayouts/ChatLayout.js
@@ -37,7 +37,8 @@ function ChatLayout() {
           axios.post("https://connectus-4ev0.onrender.com/messages/create-message", data, { headers },)
             .then((response) => {
                 toast.success("message sent")
-                messageData.newMessage = ""
+                document.getElementById("messageForm").value = ""
+
             })
             .catch((err) => {
               if (err.response) {
@@ -60,6 +61,20 @@ function ChatLayout() {
         }
       };
     
+      const handleDelete = (selectedMessage) => {
+        // console.log(selectedMessage.id)
+        axios.delete(`https://connectus-4ev0.onrender.com/messages/${selectedMessage.id}`, { headers })
+          .then(response => {
+            console.log('DELETE request successful:', response.data);
+          })
+          .catch(error => {
+            console.error('DELETE request failed:', error);
+          });
+      }
+
+
+
+
     useEffect(() => {
         const fetchData = async () => {
         try {
@@ -80,16 +95,20 @@ function ChatLayout() {
                 <main className="bg-white h-screen ml-2 w-full">
                     <div className="w-full ml-1 bg-white w-full">
                         {messages.map(message => { 
-                            return  <div key={message.id} className="bg-pink-50 py-2 px-3 mx-4 my-8 rounded-sm">
-                            <p className="text-lg">{message.content}</p> 
-                            <div className="flex ">
-                            <p className="text-xs mr-2">{message.sender.firstName}</p>
-                            <p className="text-xs">{message.sender.lastName}</p>
-                            </div>
+                            return  <div key={message._id} className="bg-pink-50 py-4 flex items-center justify-between max-w-2/3 px-6 mx-4 my-4 rounded-sm">
+                                <div>
+                                    <p className="text-lg pb-1">{message.content}</p> 
+                                    <div className="flex ">
+                                        <p className="text-xs mr-2">{message.sender.firstName}</p>
+                                        <p className="text-xs">{message.sender.lastName}</p>
+                                    </div>
+                                </div>
+                                <button onClick={() => handleDelete(message)} className="bg-orange-200 text-pink-700 font-semibold h-8 px-4 rounded-lg">D</button>
                             </div>
                         })}
-                        <div className="absolute items-center flex justify-between mb-2 bottom-0 bg-pink-50 w-3/4 mt-4 px-5 rounded-md">
+                        <div className="absolute items-center flex justify-between mb-2 bottom-0 bg-pink-50 w-2/3 mt-4 px-5 rounded-md">
                             <input 
+                                id="messageForm"
                                  value={messageData.newMessage}
                                  onChange={handleInputChange}
                                  type="text"
